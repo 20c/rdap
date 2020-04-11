0549cc53fe7141b5d66466e2229a2bba1946e405
Matt Griswold <grizz@20c.com>
2020-04-11 00:03:50 +0000

    run 2to3 plus travis

--- rdap/__init__.py
+++ rdap/__init__.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import
+
 
 from pkg_resources import get_distribution
 
--- setup.py
+++ setup.py
@@ -25,12 +25,10 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet',
         'Topic :: Software Development :: Libraries',
     ],
